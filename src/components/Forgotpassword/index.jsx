5e9221d68699510e138e43d37e3ab4cad464f812
FEAvansh <108047738+FEAvansh@users.noreply.github.com>
2022-07-29 16:23:07 +0530

    [isues]

--- src/components/Forgotpassword/index.jsx
+++ src/components/Forgotpassword/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Register/Register.css";
 import { validEmail } from "../../utils/helper";
